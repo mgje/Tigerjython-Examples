103484e16f8d86027484612e1c029ced9e3edebc
Martin Guggisberg <martin.guggisberg@unibas.ch>
2014-03-31 17:31:14 +0200

    new excercise

---
+++ WissenschaftlichesRechnen/K3/UmfangDreieck.py
@@ -0,0 +1,19 @@
+from math import *
+#Berechnung des Umfangs eines Dreiecks
+
+def umfang(l):
+    N = len(l)/2
+    um = 0.0
+    for i in range(N-1):
+        quad =  sqrt((l[2*i]  -l[2*(i+1)])**2\\
+               +(l[2*i+1]-l[2*(i+1)+1])**2)
+        um += quad
+        print um
+    
+    quad =  sqrt((l[0]  -l[-2])**2\\
+               +(l[1]   -l[-1])**2)
+    um += quad           
+    return um
+        
+# Test mit Parametern (1,0) (3,0) (3,2)
+print umfang([1.,0.,3.,0.,3.,2.])
