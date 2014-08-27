7bc63098d46f2ae5ba167102e8a7f7d53eea4228
Martin Guggisberg <martin.guggisberg@gmail.com>
2014-08-27 14:16:37 +0200

    helligkeit, scherung

--- ImageProcessing/helligkeit_erhoehen.py
+++ ImageProcessing/helligkeit_erhoehen.py
@@ -1,5 +1,6 @@
 ﻿from gpanel import *
 import os
+from time import sleep
 
 sep = os.sep
 pathProg = globals()["__file__"]
@@ -17,19 +18,21 @@ window(0, size, size, 0)    # y axis downwards
 bmIn = getImage("sprites/frogbw.png")
 image(bmIn, 0, size)
 w = bmIn.getWidth()
-h = bmIn.getHeight()
+h = bmIn.getHeight() 
 bmOut = GBitmap(w, h)
 
 
 for x in range(0, w):
+    print x
     for y in range(0, h):
         c = bmIn.getPixelColor(x, y)
-        h = c.getRed()
-        vnew = (16*h**0.5)
+        hvalue = c.getRed()
+        vnew = (16*hvalue**0.5)
         vnew = int(vnew)
         vnew = max(vnew, 0)
         vnew = min(vnew, 255)
         gray = Color(vnew, vnew, vnew)
+
         bmOut.setPixelColor(x, y, gray)
 
 image(bmOut, size / 2, size)
